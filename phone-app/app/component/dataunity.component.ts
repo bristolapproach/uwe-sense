92a8c59d40ff31540808ecb4a520d28b1b97de0e
novucs <contact@novucs.net>
2018-01-16 12:09:15 +0000

    Apply custom useragent to allow signing into dataunity via webview

--- phone-app/app/component/dataunity.component.ts
+++ phone-app/app/component/dataunity.component.ts
@@ -4,6 +4,9 @@ import {ActivatedRoute} from "@angular/router";
 import {RouterExtensions} from "nativescript-angular";
 import {ApiService} from "../app.service";
 import * as platformModule from "tns-core-modules/platform";
+import {WebViewUtils} from 'nativescript-webview-utils';
+import {WebView} from 'tns-core-modules/ui/web-view';
+import * as observable from 'tns-core-modules/data/observable';
 
 @Component({
     selector: "ns-items",
@@ -21,5 +24,10 @@ export class DataUnityComponent {
 
     back(): void {
         this.routerExtensions.back();
+    }
+
+    applyUserAgent(args: observable.EventData) {
+        const webView: WebView = <WebView>args.object;
+        WebViewUtils.setUserAgent(webView, "Mozilla/5.0 Google");
     }
 }
